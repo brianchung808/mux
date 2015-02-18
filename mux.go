6637709dd9a5e855f392ec1a62c1c1e2e74292a9
Brian Chung <brianchung23@gmail.com>
2015-02-17 22:22:45 -0800

    add comments

--- mux.go
+++ mux.go
@@ -1,3 +1,4 @@
+// Package mux provides a http request multiplexer
 package mux
 
 import (
@@ -19,16 +20,6 @@ const (
 	NUM_VERBS
 )
 
-func (r router) HandleAll(path string, endpoint Endpoint) {
-	r.Get(path, endpoint.Get)
-	r.Post(path, endpoint.Post)
-	r.Put(path, endpoint.Put)
-	r.Patch(path, endpoint.Patch)
-	r.Delete(path, endpoint.Delete)
-	r.Options(path, endpoint.Options)
-	r.Head(path, endpoint.Head)
-}
-
 // struct for user's to register all of their endpoints at once
 type Endpoint struct {
 	Get     http.HandlerFunc
@@ -40,50 +31,26 @@ type Endpoint struct {
 	Head    http.HandlerFunc
 }
 
+// type route represents a route to the path with the specified handlers
 type route struct {
 	path      string
 	endpoints []http.Handler
 }
 
+// type router holds the routes that are being handled
 type router struct {
 	// (path_URI -> route) map.
 	routes map[string]*route
 }
 
+// return a new router
 func NewRouter() *router {
 	return &router{
 		routes: make(map[string]*route),
 	}
 }
 
-func (r *router) Get(path string, handler http.Handler) {
-	r.handle(path, GET, handler)
-}
-
-func (r *router) Post(path string, handler http.Handler) {
-	r.handle(path, POST, handler)
-}
-
-func (r *router) Delete(path string, handler http.Handler) {
-	r.handle(path, DELETE, handler)
-}
-
-func (r *router) Put(path string, handler http.Handler) {
-	r.handle(path, PUT, handler)
-}
-
-func (r *router) Patch(path string, handler http.Handler) {
-	r.handle(path, PATCH, handler)
-}
-
-func (r *router) Options(path string, handler http.Handler) {
-	r.handle(path, OPTIONS, handler)
-}
-
-func (r *router) Head(path string, handler http.Handler) {
-	r.handle(path, HEAD, handler)
-}
-
+// register a handler for the specified path for the method
 func (r *router) handle(path string, verb int, handler http.Handler) {
 	// clean up path
 	path = cleanupPath(strings.NewReader(path))
@@ -103,8 +70,55 @@ func (r *router) handle(path string, verb int, handler http.Handler) {
 	currentRoute.endpoints[verb] = handler
 }
 
+// register handler for Get
+func (r *router) Get(path string, handler http.Handler) {
+	r.handle(path, GET, handler)
+}
+
+// register handler for Post
+func (r *router) Post(path string, handler http.Handler) {
+	r.handle(path, POST, handler)
+}
+
+// register handler for Delete
+func (r *router) Delete(path string, handler http.Handler) {
+	r.handle(path, DELETE, handler)
+}
+
+// register handler for Put
+func (r *router) Put(path string, handler http.Handler) {
+	r.handle(path, PUT, handler)
+}
+
+// register handler for Patch
+func (r *router) Patch(path string, handler http.Handler) {
+	r.handle(path, PATCH, handler)
+}
+
+// register handler for Options
+func (r *router) Options(path string, handler http.Handler) {
+	r.handle(path, OPTIONS, handler)
+}
+
+// register handler for Head
+func (r *router) Head(path string, handler http.Handler) {
+	r.handle(path, HEAD, handler)
+}
+
+// register handler for specified method
 func (r *router) HandleFunc(path string, verb int, handler http.HandlerFunc) {
 	r.handle(path, verb, handler)
+}
+
+// register multiple handlers at once with an Endpoint struct
+func (r router) HandleAll(path string, endpoint Endpoint) {
+	r.Get(path, endpoint.Get)
+	r.Post(path, endpoint.Post)
+	r.Put(path, endpoint.Put)
+	r.Patch(path, endpoint.Patch)
+	r.Delete(path, endpoint.Delete)
+	r.Options(path, endpoint.Options)
+	r.Head(path, endpoint.Head)
 }
 
 // satisfy http.Handler interface
@@ -153,6 +167,7 @@ func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	handler.ServeHTTP(w, req)
 }
 
+// helper to cleanup user inputted path string
 func cleanupPath(path io.Reader) string {
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(path)
